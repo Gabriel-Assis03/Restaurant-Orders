0ca3477071b768509382f4fbf52d4b43b8a1b308
Gabriel do Carmo Assis <gabrieleboltffggff@gmail.com>
2024-02-19 22:31:13 -0300

    req 1 test

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,25 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient = Ingredient('farinha')
+    ingreTest = {
+        'farinha',
+        Restriction.GLUTEN
+    }
+    assert ingredient.name == 'farinha'
+    assert ingredient.restrictions == {Restriction.GLUTEN}
+    assert ingredient.__hash__() == hash('farinha')
+    assert ingredient.__eq__(ingreTest) == True
+    assert ingredient.__repr__() == "Ingredient('farinha')"
+
+def test_ingredient2():
+    ingreTest = {
+        'farinha',
+        Restriction.GLUTEN
+    }
+    ingredient2 = Ingredient('carne')
+    assert ingredient2.__eq__(ingreTest) == False
+
